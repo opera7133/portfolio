132cf73cf8884a70dfcb1b769b9087e79e2da651
opera7133 <ainznino@pm.me>
2022-03-26 15:32:28 +0900

    preload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home: Component<{}> = () => {
       <div class="flex flex-col justify-center h-screen">
         <div class="container mx-auto max-w-5xl z-20">
           <div class="mx-4 flex flex-col md:flex-row md:items-center gap-3 md:gap-6">
-            <img src="/img/wamo.jpg" class="rounded-full w-36 shadow-md" />
+            <img src="/img/wamo.jpg" alt="usericon" class="rounded-full w-36 shadow-md" />
             <div>
               <h2 class="text-5xl pb-2 font-semibold font-futura">wamo</h2>
               <p class="text-3xl pb-4 font-futura">I make websites and videos as a hobby.</p>
