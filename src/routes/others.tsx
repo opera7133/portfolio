419c10680469d02d5076d431be80c1517a0a1c68
wamo <ainznino@pm.me>
2024-06-25 10:20:32 +0900

    interests

--- src/routes/others.tsx
+++ src/routes/others.tsx
@@ -2,7 +2,7 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { A } from "@solidjs/router";
 import { VsServer } from "solid-icons/vs";
 import { IoMail } from "solid-icons/io";
-import { RiDocumentFileEditLine } from "solid-icons/ri";
+import { RiDocumentFileEditLine, RiOthersLightbulbFill } from "solid-icons/ri";
 
 const Others = () => {
   return (
@@ -14,7 +14,7 @@ const Others = () => {
         <div class="container mx-auto max-w-7xl pb-20 z-20">
           <div class="mx-4">
             <h2 class="text-3xl mb-4 font-semibold font-futura">Others</h2>
-            <div class="flex flex-col md:flex-row gap-2 md:items-start">
+            <div class="flex flex-col md:flex-row flex-wrap gap-2 md:items-start">
               <A
                 href="/services"
                 class="bg-black px-8 py-6 md:h-40 text-white rounded-lg flex flex-col gap-2"
@@ -38,6 +38,14 @@ const Others = () => {
                 <RiDocumentFileEditLine class="fill-white" size={48} />
                 <h3 class="text-xl font-bold">Commission</h3>
                 <p>サイト・アプリ等制作の依頼について</p>
+              </A>
+              <A
+                href="/interests"
+                class="bg-black px-8 py-6 md:h-40 text-white rounded-lg flex flex-col gap-2"
+              >
+                <RiOthersLightbulbFill class="fill-white" size={48} />
+                <h3 class="text-xl font-bold">Interests</h3>
+                <p>最近聞いてる音楽とか</p>
               </A>
             </div>
           </div>
