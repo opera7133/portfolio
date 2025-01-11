e42c99176cb8170802d7c53e0a79c974c726e86f
wamο <ainznino@pm.me>
2025-01-11 18:03:26 +0900

    Update interests.tsx

--- src/routes/interests.tsx
+++ src/routes/interests.tsx
@@ -11,216 +11,247 @@ const Interests = () => {
         <div class="container mx-auto max-w-7xl z-20">
           <div class="mx-4">
             <h2 class="text-3xl mb-4 font-semibold font-futura">Interests</h2>
-            <h3 class="text-2xl font-semibold">音楽</h3>
-            <p class="my-4">
-              2023年5月から、月ごとに聞いた音楽をプレイリストにまとめています。
-            </p>
-            <h4 class="text-xl font-semibold">2024年</h4>
-            <ul class="list-disc ml-8">
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/49uD6qgHOB3YflENXhyqNr?si=9bbedec75a2349c1"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年1月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/23s5qwBASu3HWp0RaTcbyB?si=e94b048239724eb3"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年2月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/3pS7di4zO6OJ3fFaXzgvxQ?si=8006df06b53543b5"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年3月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/5YYukCWTcj99mQuZEVBZpJ?si=8f75d0877fa94cc0"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年4月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/4i6f1vv8JkNS6hagpGRLN2?si=b3be451646a4470d"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年5月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/3VotBaESno4wTfFXjbuKvf?si=577c159c65b148d5"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年6月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/4BVILUEwsY4t0ZTAsBY5nX?si=Fhlbm-e0SgW4hGjtrhzGSg&pi=V49vuAFgRP6-z"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年7月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/2ZeOKXTSszTJEMuORln56a?si=a7c97f26fc014b88"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年8月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/5Poygb7Uen6SnN5sPHa3TY?si=a89df44e35d84bf4"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年9月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/0DdpJQOR4PlRKhPIuncTKK?si=5211fdd7b5614471"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年10月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/2A4Nk3lICjOThTXlUNuKLv?si=f77ef48ff2e14a98"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年11月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/3SYnU8sfxK7ulVrkSYybVm?si=987463afdd3247e0"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2024年12月
-                </A>
-              </li>
-            </ul>
-            <h4 class="text-xl font-semibold">2023年</h4>
-            <ul class="list-disc ml-8">
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/1uO49QOIsJ30zaFJMfGoZW?si=8d1d4f5866964cd0"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年5月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/6jWT0rzvMOOAp9f6J4IZnm?si=3c04221222774a3a"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年6月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/47hoK0bxEKx9TS9QdXALBv?si=8b6954fd0c914ffd"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年7月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/6mQFQPONzVOvCtcHtrUIP7?si=92ccf9a0cfdb4926"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年8月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/1azw7qKBjuhy3ogwaA59V3?si=46d6de4e6d2d423e"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年9月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/56nBqJIXhNcqxwdyA6jRlk?si=b402b63410854f87"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年10月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/0IBBqDPVHTJlpMXjowK9UT?si=64db1ff4745d4323"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年11月
-                </A>
-              </li>
-              <li>
-                <A
-                  class="underline"
-                  href="https://open.spotify.com/playlist/5oFo9eaVEHEARbf5VryQMN?si=e61cd2c7efd64557"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  2023年12月
-                </A>
-              </li>
-            </ul>
+            <div class="my-4" id="musics">
+              <h3 class="text-2xl font-semibold">音楽</h3>
+              <p class="my-4">
+                2023年5月から、月ごとに聞いた音楽をプレイリストにまとめています。
+              </p>
+              <h4 class="text-xl font-semibold">2025年</h4>
+              <ul class="list-disc ml-8">
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/4sv4HsZSPM39TKpViFhqjP?si=ecae1d8462d54171"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2025年1月
+                  </A>
+                </li>
+              </ul>
+              <h4 class="text-xl font-semibold">2024年</h4>
+              <ul class="list-disc ml-8">
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/49uD6qgHOB3YflENXhyqNr?si=9bbedec75a2349c1"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年1月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/23s5qwBASu3HWp0RaTcbyB?si=e94b048239724eb3"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年2月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/3pS7di4zO6OJ3fFaXzgvxQ?si=8006df06b53543b5"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年3月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/5YYukCWTcj99mQuZEVBZpJ?si=8f75d0877fa94cc0"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年4月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/4i6f1vv8JkNS6hagpGRLN2?si=b3be451646a4470d"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年5月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/3VotBaESno4wTfFXjbuKvf?si=577c159c65b148d5"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年6月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/4BVILUEwsY4t0ZTAsBY5nX?si=Fhlbm-e0SgW4hGjtrhzGSg&pi=V49vuAFgRP6-z"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年7月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/2ZeOKXTSszTJEMuORln56a?si=a7c97f26fc014b88"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年8月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/5Poygb7Uen6SnN5sPHa3TY?si=a89df44e35d84bf4"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年9月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/0DdpJQOR4PlRKhPIuncTKK?si=5211fdd7b5614471"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年10月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/2A4Nk3lICjOThTXlUNuKLv?si=f77ef48ff2e14a98"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年11月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/3SYnU8sfxK7ulVrkSYybVm?si=987463afdd3247e0"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2024年12月
+                  </A>
+                </li>
+              </ul>
+              <h4 class="text-xl font-semibold">2023年</h4>
+              <ul class="list-disc ml-8">
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/1uO49QOIsJ30zaFJMfGoZW?si=8d1d4f5866964cd0"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年5月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/6jWT0rzvMOOAp9f6J4IZnm?si=3c04221222774a3a"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年6月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/47hoK0bxEKx9TS9QdXALBv?si=8b6954fd0c914ffd"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年7月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/6mQFQPONzVOvCtcHtrUIP7?si=92ccf9a0cfdb4926"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年8月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/1azw7qKBjuhy3ogwaA59V3?si=46d6de4e6d2d423e"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年9月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/56nBqJIXhNcqxwdyA6jRlk?si=b402b63410854f87"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年10月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/0IBBqDPVHTJlpMXjowK9UT?si=64db1ff4745d4323"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年11月
+                  </A>
+                </li>
+                <li>
+                  <A
+                    class="underline"
+                    href="https://open.spotify.com/playlist/5oFo9eaVEHEARbf5VryQMN?si=e61cd2c7efd64557"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    2023年12月
+                  </A>
+                </li>
+              </ul>
+            </div>
+            <div class="my-4" id="transit">
+              <h3 class="text-2xl font-semibold">交通系ICカード</h3>
+              <p class="my-4">
+                交通系ICカードの収集にハマっています。
+              </p>
+              <ul class="list-disc ml-8">
+                <li>Suica</li>
+                <li>Kitaca</li>
+                <li>はやかけん</li>
+                <li>nimoca</li>
+                <li>SUGOCA</li>
+                <li>totra</li>
+                <li>PiTaPa</li>
+                <li>N+カード</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
