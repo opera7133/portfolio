ba3c9a5b506702d8c1b2c1e90e423335964f5722
wamo <ainznino@pm.me>
2025-02-17 14:11:41 +0900

    interests data

---
+++ src/routes/interests.data.ts
@@ -0,0 +1,30 @@
+export const spotifyPlaylist = {
+  2023: {
+    5: "https://open.spotify.com/playlist/1uO49QOIsJ30zaFJMfGoZW?si=8d1d4f5866964cd0",
+    6: "https://open.spotify.com/playlist/6jWT0rzvMOOAp9f6J4IZnm?si=3c04221222774a3a",
+    7: "https://open.spotify.com/playlist/47hoK0bxEKx9TS9QdXALBv?si=8b6954fd0c914ffd",
+    8: "https://open.spotify.com/playlist/6mQFQPONzVOvCtcHtrUIP7?si=92ccf9a0cfdb4926",
+    9: "https://open.spotify.com/playlist/1azw7qKBjuhy3ogwaA59V3?si=46d6de4e6d2d423e",
+    10: "https://open.spotify.com/playlist/56nBqJIXhNcqxwdyA6jRlk?si=b402b63410854f87",
+    11: "https://open.spotify.com/playlist/0IBBqDPVHTJlpMXjowK9UT?si=64db1ff4745d4323",
+    12: "https://open.spotify.com/playlist/5oFo9eaVEHEARbf5VryQMN?si=e61cd2c7efd64557",
+  },
+  2024: {
+    1: "https://open.spotify.com/playlist/49uD6qgHOB3YflENXhyqNr?si=9bbedec75a2349c1",
+    2: "https://open.spotify.com/playlist/23s5qwBASu3HWp0RaTcbyB?si=e94b048239724eb3",
+    3: "https://open.spotify.com/playlist/3pS7di4zO6OJ3fFaXzgvxQ?si=8006df06b53543b5",
+    4: "https://open.spotify.com/playlist/5YYukCWTcj99mQuZEVBZpJ?si=8f75d0877fa94cc0",
+    5: "https://open.spotify.com/playlist/4i6f1vv8JkNS6hagpGRLN2?si=b3be451646a4470d",
+    6: "https://open.spotify.com/playlist/3VotBaESno4wTfFXjbuKvf?si=577c159c65b148d5",
+    7: "https://open.spotify.com/playlist/4BVILUEwsY4t0ZTAsBY5nX?si=Fhlbm-e0SgW4hGjtrhzGSg",
+    8: "https://open.spotify.com/playlist/2ZeOKXTSszTJEMuORln56a?si=a7c97f26fc014b88",
+    9: "https://open.spotify.com/playlist/5Poygb7Uen6SnN5sPHa3TY?si=a89df44e35d84bf4",
+    10: "https://open.spotify.com/playlist/0DdpJQOR4PlRKhPIuncTKK?si=5211fdd7b5614471",
+    11: "https://open.spotify.com/playlist/2A4Nk3lICjOThTXlUNuKLv?si=f77ef48ff2e14a98",
+    12: "https://open.spotify.com/playlist/3SYnU8sfxK7ulVrkSYybVm?si=987463afdd3247e0",
+  },
+  2025: {
+    1: "https://open.spotify.com/playlist/4sv4HsZSPM39TKpViFhqjP?si=ecae1d8462d54171",
+    2: "https://open.spotify.com/playlist/6G5KK9UadvicOiFEdivj59?si=ef7c06c5f1464b88",
+  },
+};
