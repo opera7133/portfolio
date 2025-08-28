abc7260a8beeedabe99747f027d3bd7d82afbbd0
wamo <ainznino@pm.me>
2025-08-29 00:54:19 +0900

    Update interests.ts

--- src/data/interests.ts
+++ src/data/interests.ts
@@ -31,5 +31,6 @@ export const spotifyPlaylist = {
     5: "https://open.spotify.com/playlist/2mjSoEy9mW2AOQtNFpkitc?si=4a2d32e8dd4244f0",
     6: "https://open.spotify.com/playlist/3B6jUWF5Addtav6bBYEXks?si=76d6db5671564fee",
     7: "https://open.spotify.com/playlist/0GrAyMSSqpggc0lRPWLvLy?si=2789148ec2bd457e",
+    8: "https://open.spotify.com/playlist/4qibPP1NFyCPx0JTjShmoh?si=d4c3a1aa3ba44786"
   },
 };
